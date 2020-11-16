5303e35724cebb02c15e8ce35d0e777122723af9
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-16 13:03:57 +0530

    lambda-commit

--- src/com/inter/Caller.java
+++ src/com/inter/Caller.java
@@ -2,12 +2,32 @@ package com.inter;
 
 public class Caller {
   public static void call() {
-	  LibraryClass.LibraryMethod(new Temp());
-	  LibraryClass.LibraryMethod(new Temp2());
+	  
+	  //anonymous class
+	  LibraryClass.LibraryMethod(new Task() {
+
+		@Override
+		public void activity(String name) {
+			// TODO Auto-generated method stub
+			System.out.println("running"+name);
+			System.out.println("Caller activity done");
+		}
+		  
+	  });
+	  
+	  
+	  //lambda syntax
+	  System.out.println("--------------------------------------");
+	  LibraryClass.LibraryMethod( (name)-> {
+
+			System.out.println("RUNNING"+name);
+			System.out.println("CCCAALLLER activity done");
+		  
+	  });
   }
 }
 
-
+/*
 class Temp implements Task{
 
 	@Override
@@ -28,4 +48,4 @@ class Temp2 implements Task{
 		System.out.println("CCCAALLLER activity done");
 	}
 	
-}
+}*/
--- src/com/inter/Task.java
+++ src/com/inter/Task.java
@@ -3,3 +3,13 @@ package com.inter;
 public interface Task {
    public void activity(String name);
 }
+
+
+//lambda expressions
+//lambda expression comes in picture only if we have functional interface
+//in place
+//interface with one  method is what we call funtional interface
+
+//lambda expression is a handy and alternate syntax for anonymous class
+
+// but applicable if and only the anonymous class has one method
