fa750b559ca9322cecf8150e6fa6e85a23894159
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-17 12:29:26 +0530

    movie-filtering-on-rating

--- src/com/streams/MoviesFilter.java
+++ src/com/streams/MoviesFilter.java
@@ -3,13 +3,16 @@ package com.streams;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class MoviesFilter {
+ public static Predicate<String> movierating=MoviesFilter::filterIt;
    public static void main(String n[]) {
 	   try {
 			Stream<String> stream = Files.lines(Paths.get("d:\\movies.csv"));
-			stream.forEach(MoviesFilter::processIt);
+			//stream.forEach(MoviesFilter::processIt);
+			stream.filter(movierating).forEach(MoviesFilter::processIt);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -19,5 +22,17 @@ public class MoviesFilter {
    public static void processIt(String x) {
 	   String n[] =x.split(",");
 	   System.out.printf("\n%40s %10s" ,n[0],n[3]);
+   }
+   
+   public static boolean filterIt(String x) {
+	   String n[] =x.split(",");
+	 //  System.out.println(Integer.parseInt(n[3])>75?true:false);
+	   try {
+	   return Integer.parseInt(n[3])>82?true:false;
+	   }
+	   catch(NumberFormatException e) {
+		   return false;
+	   }
+	  
    }
 }
